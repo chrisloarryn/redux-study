096f574d2950d8f0c4382ab7ddad31db6e1c237b
chrisloarryn <chrisloarryn@gmail.com>
2020-12-02 22:19:51 -0300

    dispatch actions and reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import store from './store';
+import { bugAdded, bugResolved } from './actions';
 
-store.dispatch({
-  type: 'bugAdded',
-  payload: {
-    description: 'Bug1'
-  }
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed', store.getState());
 });
 
+store.dispatch(bugAdded('Bug1'));
+store.dispatch(bugAdded('Bug2'));
+store.dispatch(bugAdded('Bug3'));
+store.dispatch(bugAdded('Bug4'));
+store.dispatch(bugResolved(1));
+
 console.log(store.getState());
+unsubscribe();
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1
+//   }
+// })
+
+// console.log(store.getState())
